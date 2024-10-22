c763087e673fd9fd95ffdaa2b51f22c849b5d7ae
Joao-Miranda-jvcm <joaovictorcamargodemiranda@gmail.com>
2024-10-21 22:24:40 -0300

    removi o cadastro de atleta e categoria do cadastroInscricaoBean e cadastroInscricao, coloquei para selecionar o atleta e a categoria pelo selectonemenu

--- src/main/java/com/curso/controller/CadastroInscricaoBean.java
+++ src/main/java/com/curso/controller/CadastroInscricaoBean.java
@@ -3,23 +3,15 @@ package com.curso.controller;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
-
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
-
-import org.primefaces.event.ToggleEvent;
-import org.primefaces.model.Visibility;
-
 import com.curso.modelo.Atleta;
 import com.curso.modelo.Categoria;
-import com.curso.modelo.Clube;
 import com.curso.modelo.Evento;
 import com.curso.modelo.Inscricao;
 import com.curso.modelo.enums.Status;
-import com.curso.service.AtletaService;
-import com.curso.service.CategoriaService;
 import com.curso.service.InscricaoService;
 import com.curso.util.MessageUtil;
 import com.curso.util.NegocioException;
@@ -36,38 +28,12 @@ public class CadastroInscricaoBean implements Serializable {
 	private List<Status> statuss;
 	private List<Categoria> categorias;
 	private List<Evento> eventos;
-	private List<Clube> clubes;
-	private Atleta atleta;
-	private Categoria categoria;
-	private boolean cadastroAtletaAtivo;
-	private boolean cadastroCategoriaAtivo;
 	
 	@Inject
 	private InscricaoService inscricaoService;
-	@Inject
-	private AtletaService atletaService;
-	@Inject
-	private CategoriaService categoriaService;
 	
 	public void salvar() {
 		try {
-			
-			if(cadastroAtletaAtivo) {
-				String email = this.atleta.getEmail();
-				this.atletaService.salvar(atleta);
-				Atleta atletaSelecionado = atletaService.buscarPeloEmail(email);
-				inscricao.setAtleta(atletaSelecionado);
-			}else {
-				this.atleta=null;
-			}
-			if(cadastroCategoriaAtivo) {
-				String sigla = this.categoria.getSigla();
-				this.categoriaService.salvar(categoria);
-				Categoria categoriaSelecionada = categoriaService.buscarPelaSigla(sigla);
-				inscricao.setCategoria(categoriaSelecionada);
-			}else {
-				this.categoria=null;
-			}
 			this.inscricaoService.salvar(inscricao);
 			MessageUtil.sucesso("Inscrição salva com sucesso!");
 		} catch (NegocioException e) {
@@ -75,10 +41,7 @@ public class CadastroInscricaoBean implements Serializable {
 		} catch (Exception ex) {
 			MessageUtil.erro(ex.getMessage());
 		}
-		
 		this.limpar();
-		cadastroAtletaAtivo=false;
-		cadastroCategoriaAtivo=false;
 	}
 	
 	@PostConstruct
@@ -87,7 +50,6 @@ public class CadastroInscricaoBean implements Serializable {
 		atletas = inscricaoService.buscarAtletas();
 		statuss = Arrays.asList(Status.values());
 		categorias = inscricaoService.buscarCategoria();
-		clubes = atletaService.buscarClubes();
 		limpar();
 	}
 	
@@ -118,68 +80,4 @@ public class CadastroInscricaoBean implements Serializable {
 	public List<Evento> getEventos() {
 		return eventos;
 	}
-
-	public Atleta getAtleta() {
-		return atleta;
-	}
-	public String getEmailAtleta() {
-		return this.atleta.getEmail();
-	}
-
-	public void setAtleta(Atleta atleta) {
-		this.atleta = atleta;
-	}
-	public Categoria getCategoria() {
-		return categoria;
-	}
-
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
-	}
-
-	public AtletaService getAtletaService() {
-		return atletaService;
-	}
-
-	public void setAtletaService(AtletaService atletaService) {
-		this.atletaService = atletaService;
-	}
-	public CategoriaService getCategoriaService() {
-		return categoriaService;
-	}
-
-	public void setCategoriaService(CategoriaService categoriaService) {
-		this.categoriaService = categoriaService;
-	}
-	public List<Clube> getClubes() {
-		return atletaService.buscarClubes();
-	}
-	public boolean isCadastroAtletaAtivo() {
-        return cadastroAtletaAtivo;
-    }
-	public boolean isCadastroCategoriaAtivo() {
-		return cadastroCategoriaAtivo;
-	}
-    public void setCadastroAtletaAtivo(boolean cadastroAtletaAtivo) {
-        this.cadastroAtletaAtivo = cadastroAtletaAtivo;
-    }
-    public void onPainelAtletaToggled(ToggleEvent event) {
-        if (event.getVisibility() == Visibility.VISIBLE) {
-            cadastroAtletaAtivo = true; 
-            this.atleta = new Atleta();
-        } else {
-            cadastroAtletaAtivo = false;
-
-        }
-    }
-    public void onPainelCategoriaToggled(ToggleEvent event) {
-        if (event.getVisibility() == Visibility.VISIBLE) {
-            cadastroCategoriaAtivo = true;  
-            this.categoria = new Categoria();
-        } else {
-            cadastroCategoriaAtivo = false;
-        }
-    }
-	
-	
 }
